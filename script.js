b4b4dffac43500f892427cea8b83d0a051a517d1
ishak <ishaksylmz7@gmail.com>
2024-06-27 11:59:19 +0300

    main

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-const inputEl = document.querySelector('#email');
-const btn = document.querySelector('#submit');
+const inputEl = document.querySelector("#email");
+const btn = document.querySelector("#submit");
 const RegEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-const iconErrorEl = document.querySelector('.icon-error');
-const textErrorEl = document.querySelector('.text-error');
-const fromGroupEl = document.querySelector('.form-group');
+const iconErrorEl = document.querySelector(".icon-error");
+const textErrorEl = document.querySelector(".text-error");
+const fromGroupEl = document.querySelector(".form-group");
 let errors = [];
 
-btn.addEventListener('click', e => {
-    e.preventDefault();
-  textErrorEl.classList.remove('text-success');
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  textErrorEl.classList.remove("text-success");
 
   errors = [];
-  
+
   const email = inputEl.value;
 
-  if (email == '' || email === undefined) {
-    errors.push('Please provide us your email');
+  if (email == "" || email === undefined) {
+    errors.push("Please provide us your email");
     // show error
-    iconErrorEl.style.display = 'block';
+    iconErrorEl.style.display = "block";
     textErrorEl.style.display = "block";
     textErrorEl.innerText = errors[0];
   } else if (!email.match(RegEmail)) {
-    errors.push('Please provide us your valid email');
+    errors.push("Please provide us your valid email");
     // show error icon
-    iconErrorEl.style.display = 'block';
+    iconErrorEl.style.display = "block";
     textErrorEl.style.display = "block";
     textErrorEl.innerText = errors[0];
   }
 
   if (!errors.length > 0) {
     // hide error icon
-    iconErrorEl.style.display = 'none';
+    iconErrorEl.style.display = "none";
     textErrorEl.style.display = "none";
-    textErrorEl.classList.add('text-success');
-    textErrorEl.innerText = 'Thank you for subscribing to our newsletter.';
+    textErrorEl.classList.add("text-success");
+    textErrorEl.innerText = "Thank you for subscribing to our newsletter.";
   }
 });
